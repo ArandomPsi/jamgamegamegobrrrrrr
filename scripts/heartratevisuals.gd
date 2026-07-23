604a5a482e39229c485fbf3e248163cd4101bed0
Kosm0-o <logantreboutat@gmail.com>
2026-07-22 20:48:57 -0700

    hearratevisualllsslineeees

---
+++ scripts/heartratevisuals.gd
@@ -0,0 +1,37 @@
+extends Line2D
+
+var heartrate : int = randi_range(5, 35) * [1, -1].pick_random()
+var graph : Curve = Curve.new()
+var pointnum : int = 0
+var delay : bool = false
+
+func _ready() -> void:
+	randomize()
+	graph.min_domain = -88.5
+	graph.max_domain = 88.5
+	graph.min_value = -250.0
+	graph.max_value = 250.0
+	var space = (graph.get_domain_range() - 15.06 * 3) / 4
+	var amt = randi_range(1, 3)
+	for i in range(amt):
+		space += randi_range(-21, 75) / amt
+		var startx = (i + 1) * space + graph.min_domain
+		var endx = startx + 15.06
+		graph.add_point(Vector2(startx, graph.min_value))
+		graph.add_point(Vector2((startx + endx) / 2, graph.max_value))
+		graph.add_point(Vector2(endx, graph.min_value))
+		
+
+func _process(delta: float) -> void:
+	pointnum += 5
+	if pointnum > graph.get_domain_range():
+		pointnum = 1
+		delay = !delay
+	if !delay:
+		clear_points()
+		for i in range(pointnum):
+			var x = i + graph.min_domain
+			var y = (-graph.sample(x) / 8) + (93.5 * 1.8) + graph.min_value
+			if y < -78.0:
+				y -= heartrate / 1.5
+			add_point(Vector2(x, y))
