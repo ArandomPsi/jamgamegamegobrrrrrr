44b0dd82068cffa979170bcda92b9cf302168f71
ArandomPsi <116410822+ArandomPsi@users.noreply.github.com>
2026-07-22 18:00:27 -0700

    diddy

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 var down : bool = true
 
+func _input(event):
+	if event.is_action_pressed("esc"):
+		resetcam()
+
+
 func cam_tween(up : bool):
 	var end = 324.0 if up else 900.0
 	var tween = create_tween().set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
@@ -20,9 +25,15 @@ func _on_button_pressed() -> void:
 
 
 func resetcam():
-	cam_tween(true)
+	campostween(Vector2(575.0,324))
 	camzoomtween(1)
 	down = false
+	if down: $Button.text = "down"
+	else:$Button.text = "up"
+	
+	
+	#make everything not visible
+	$joe/eye.visible = false
 
 func camzoomtween(val : float):
 	var tween = create_tween()
@@ -34,6 +45,6 @@ func campostween(pos : Vector2):
 
 
 func _on_light_pressed() -> void:
-	campostween(Vector2(410,180))
+	campostween(Vector2(410,160))
 	camzoomtween(8)
 	$joe/eye.visible = true
